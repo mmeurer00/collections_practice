3e45a392c40d489e755d0411764a6a751e6e6294
Maxine <maxinemeurer@yahoo.com>
2021-02-05 11:29:28 -0500

    commit tree d642212f5f5f0429fe43b9035b9d8dad9006cc91

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,52 @@
+def sort_array_asc(array)
+    array.sort do |a, b|
+         a <=> b
+    end
+end
 
+def sort_array_desc(array)
+    array.sort do |a, b|
+         b <=> a
+    end
+end
+
+def sort_array_char_count(array)
+    array.sort do |a, b|
+        a.size <=> b.size
+    end
+end
+
+def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+    return array
+end
+
+def reverse_array(array)
+    return array.reverse
+end
+
+def kesha_maker(array)
+    array.each do |dolla|
+        dolla[2] = "$"
+    end
+end
+
+def find_a(array)
+    array.select do |words|
+        words[0] == "a"
+    end
+end
+
+def sum_array(array)
+    array.inject(0, :+)
+end
+
+def add_s(array)
+    array.map do |element|
+        if element != array[1]
+            element + "s"
+        else 
+            element
+        end
+    end 
+end
